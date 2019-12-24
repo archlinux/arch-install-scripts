4cb00b3c2182fed936fd4d047fc26cf7dc441208
Eli Schwartz <eschwartz@archlinux.org>
2019-12-23 19:44:07 -0500

    completion: make usergroups split properly on ':'

--- completion/arch-chroot.bash
+++ completion/arch-chroot.bash
@@ -1,9 +1,7 @@
 _arch_chroot() {
     compopt +o dirnames
     local cur prev opts
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    _init_completion -n : || return
     opts="-u -h"
 
     for i in "${COMP_WORDS[@]:1:COMP_CWORD-1}"; do
