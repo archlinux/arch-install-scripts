8be93400011fa45dd4048707c7bfa8bdc2cb3202
Mike Yuan <me@yhndnzj.com>
2022-08-20 02:04:41 +0800

    pacstrap: add option '-K' to initialize an empty keyring in the new root

--- completion/pacstrap.bash
+++ completion/pacstrap.bash
@@ -8,7 +8,7 @@ _pacstrap() {
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="-C -c -G -i -M -N -h"
+    opts="-C -c -G -i -K -M -N -h"
 
     for i in "${COMP_WORDS[@]:1:COMP_CWORD-1}"; do
         if [[ -d ${i} ]]; then
